2bc19eb0fd923ee38424b3625e4222f28bb35c9a
Github007 <jiang_ning@ckl.com>
2019-08-01 11:28:30 +0800

    almost done

--- Todo/index.js
+++ Todo/index.js
@@ -1,5 +1,6 @@
 var app = new Vue({
     el: '#todo-app',
+
     data: function(){
         return {
             todos: [
@@ -10,8 +11,11 @@ var app = new Vue({
             newTodoTitle: '',
             emptyContent:false,
             editTodo: null,
+            intention: 'all',
+            selectedButton: '',
         }
     },
+
     methods: {
         addTodo: function (){
             if(!this.newTodoTitle){
@@ -30,29 +34,79 @@ var app = new Vue({
         },
         deleteTodo: function(todo){
             const pressButton = confirm("您确定要删除该Todo吗？")
-            if(pressButton == true)
+            if(pressButton == true){
                 this.todos.splice(this.todos.indexOf(todo),1)
+                return true
+            }
+            return false
         },
         modifyTodo: function(todo){
             this.editTodo = {id: todo.id, title: todo.title}
         },
         modifyDone: function(todo){
             if(todo.title === ''){
-                this.deleteTodo(todo)
+                if(this.deleteTodo(todo)){
+                    this.editTodo = null
+                }else{
+                    this.modifyCancel(todo)
+                }
+                return
             }
             this.editTodo = null
         },
         modifyCancel: function(todo){
             todo.title = this.editTodo.title
             this.editTodo = null
-        }
+        },
+        markAllAsCompleted: function() {
+            this.todos.map( todo => {
+                if( !todo.completed ){
+                    todo.completed = true
+                }
+            })
+        },
+        clearCompleted: function(){
+            this.selectedButton = 'clearCompleted'
+            this.todos = this.todos.filter(todo => !todo.completed)
+        },
+        clearAll: function() {
+            this.selectedButton = 'clearAll'
+            this.todos = []
+        },
     },
+
     computed: {
         inputBorder: function(){
             if(this.emptyContent)
                 return 'red'
             else 
                 return ''
+        },
+        modifyFocus: function(){
+            if(this.editTodo !== null){
+                this.$refs.modifyInput.$el.focus()
+            }
+        },
+        uncompletedCount: function(){
+            return this.todos.filter( todo => !todo.completed).length
+        },
+        filteredTodos: function(){
+            if(this.intention === 'ongoing') {
+                this.selectedButton = 'ongoing'
+                return this.todos.filter( todo => !todo.completed)
+            }else if (this.intention === 'completed'){
+                this.selectedButton = 'completed'
+                return this.todos.filter( todo => todo.completed )
+            }else{
+                this.selectedButton = 'all'
+                return this.todos
+            }
+        }
+    },
+
+    directives: {
+        focus: {
+            inserted: function(el) {el.focus()}
         }
     }
 })
